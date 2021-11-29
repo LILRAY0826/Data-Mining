3953f206dd29ee1bbda83d7f9a918db4494d079c
Ray Ching <71825980+LILRAY0826@users.noreply.github.com>
2021-11-29 12:39:28 +0800

    Add files via upload

--- Bayes_Classifier/main.py
+++ Bayes_Classifier/main.py
@@ -1,19 +1,28 @@
 from sklearn.model_selection import train_test_split
 import Data_Preprocessing
 import Naive_Bayes_Classifier
+import GaussianNB
 
 # Split Data
 total_score = 0
-for i in range (0, 10):
-    print("Training : {:d} times.".format(i+1))
+model = "GaussianNB"
+
+for i in range(0, 10):
+    print("Predicting : {:d} times.".format(i + 1))
     # 10-Fold Cross-Validation
     train_data, test_data = train_test_split(Data_Preprocessing.dataframe, train_size=0.9, random_state=i)
     train_data.reset_index(inplace=True, drop=True)
     test_data.reset_index(inplace=True, drop=True)
     test_data_target = test_data["HeartDisease"]
 
-    predict_result = Naive_Bayes_Classifier.predict(train_data, test_data)
-    total_score += Naive_Bayes_Classifier.accuracy(predict_result, test_data_target)
+    if model == "Naive_Bayes_Classifier":
+        predict_result = Naive_Bayes_Classifier.predict(train_data, test_data)
+        total_score += Naive_Bayes_Classifier.accuracy(predict_result, test_data_target)
+
+    elif model == "GaussianNB":
+        predict_result = GaussianNB.predict(train_data, test_data)
+        total_score += GaussianNB.accuracy(predict_result, test_data_target)
+
 
 score = total_score/10
 print('{:.2f}%'.format(score * 100))
